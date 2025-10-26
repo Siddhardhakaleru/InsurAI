d8e5a0d359be2364aacbf616a19fb9c4b8ee76c7
Kaleru Siddhardha <121043266+Siddhardhakaleru@users.noreply.github.com>
2025-10-27 04:17:44 +0530

    Add files via upload

---
+++ JavaExercises/DataTypesExample1.java
@@ -0,0 +1,21 @@
+public class DataTypesExample1 {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 5000;
+        long l = 150000L;
+        float f = 5.75f;
+        double d = 19.99d;
+        char c = 'S';
+        boolean flag = true;
+
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + c);
+        System.out.println("Boolean value: " + flag);
+    }
+}
---
+++ JavaExercises/DataTypesExample3.java
@@ -0,0 +1,7 @@
+public class DataTypesExample3 {
+    public static void main(String[] args) {
+        float radius = 4.5f;
+        double area = 3.14159 * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
